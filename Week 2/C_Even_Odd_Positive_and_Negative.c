da98f5eb395e6584da73529f80a40f496a614c36
kfatima0 <kanizfatima662@gmail.com>
2024-10-12 23:55:44 +0600

    even_odd_pos_neg

---
+++ Week 2/C_Even_Odd_Positive_and_Negative.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a;
+    int even=0,odd=0,pos=0,neg=0;
+
+    for(int i=1;i<=n;i=i+1)
+    {
+        scanf("%d",&a);
+        if(a%2==0)
+        {
+            //even
+            even++;
+        }
+        else if(a%2==1)
+        {
+            //odd
+            odd++;
+        }
+        else if(a<0)
+        {
+            //negative
+            neg++;
+        }else if(a>0)
+        {
+            //positive
+            pos++;
+        }
+    }
+    printf("Even: %d\nOdd: %d\nPositive: %d\nNegative: %d\n",even,odd,pos,neg);
+    return 0;
+    
+}
